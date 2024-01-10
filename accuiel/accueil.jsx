e4f857abe7b4c3fd13ea2991ecdb38fb0de0b3eb
mostafa <almannanimostafa@gmail.com>
2024-01-10 14:19:29 +0100

    accuiel

---
+++ accuiel/accueil.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Accueil() {
+    return (
+        <div className='row'>
+            <div className='col-10' ></div>
+            <div className='col-2'>
+                <img  src="image acceuil.png" class="rounded float-end" alt="..."></img>
+            </div>
+                
+        </div>
+    )
+}
